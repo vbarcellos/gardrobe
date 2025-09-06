fdd6b07a9b258531d04d91e8ff34bb349e426524
Victor Barcellos <victorfsb@gmail.com>
2025-09-06 05:19:04 -0300

    fix: update import statement for store to include file extension

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
-import store from "./redux/store";
+import store from "./redux/store.js";
 import { App } from "./App.jsx";
 import "./styles.css";
 
